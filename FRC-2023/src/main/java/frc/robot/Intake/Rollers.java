e22871563f333f75a11ffa98f564ac39d89050be
Abram <abramwpierce@icloud.com>
2023-02-04 20:30:59 -0700

    Changed speed of intake

--- FRC-2023/src/main/java/frc/robot/Intake/Rollers.java
+++ FRC-2023/src/main/java/frc/robot/Intake/Rollers.java
@@ -8,6 +8,6 @@ public class Rollers {
     public static VictorSPX intakeMotor1 = new VictorSPX(20);
     
     public static void roll(double lt, double rt) {
-        intakeMotor1.set(VictorSPXControlMode.PercentOutput, (rt - lt) / 1.5);
+        intakeMotor1.set(VictorSPXControlMode.PercentOutput, (rt - lt));
     }
 }
