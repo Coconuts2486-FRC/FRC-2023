814964d15f6c33c7c19c21d1744da59e8f67ca0c
Abram <abramwpierce@icloud.com>
2023-02-27 11:56:33 -0700

    added supposed color switcher

--- FRC-2023/src/main/java/frc/robot/Map.java
+++ FRC-2023/src/main/java/frc/robot/Map.java
@@ -1,13 +1,15 @@
 package frc.robot;
 
 import com.ctre.phoenix.sensors.Pigeon2;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PowerDistribution;
 import frc.robot.Drive.Swerve;
 
 public class Map {
 
     /*
-     * JOYSTICK buttons and their controls:
+     * Driver JOYSTICK buttons and their controls:
      * (1) A                - Drive temporary auto path
      * (2) B                - Winch extends arm
      * (3) X                - Arm goes to top position
@@ -18,13 +20,31 @@ public class Map {
      * (8) Menu             - Nothing
      * (9) Left Stick       - Nothing
      * (10) Right Stick     - Nothing
+     * 
+     * CODRIVER JOYSTICK buttons and their controls:
+     * (1)
+     * (2)
+     * (3)
+     * (4)
+     * (5)
+     * (6)
+     * (7)
+     * (8)
+     * (9)
+     * (10)
      */
 
     public static double deadband = 0.1;
     public static double ticksToInches = 56.5;
     
     public static Joystick driver = new Joystick(0);
+    public static Joystick coDriver = new Joystick(1);
+
+
     public static Swerve swerve = new Swerve();
+
+    public static PowerDistribution pdp = new PowerDistribution(0, PowerDistribution.ModuleType.kRev);
+    public static boolean isRedAlliance = true;
 
     public static Pigeon2 gyro = new Pigeon2(40);
     public static double initialAngle;
@@ -34,4 +54,15 @@ public class Map {
     public static int[] driverMode = {0, 1, 4};
     public static int[] normalMode = {0, 1, 4};
     public static int[] kateMode = {4, 5, 0};
+
+    // function for changing the color of the lights
+    public static void changePDPLights(boolean switchColor) {
+        // do nothing if the button isn't pressed
+        if (!switchColor) {
+            return;
+        }
+        // switch the color and change the variable
+        pdp.setSwitchableChannel(isRedAlliance);
+        isRedAlliance = !isRedAlliance;
+    }
 }
--- FRC-2023/src/main/java/frc/robot/Robot.java
+++ FRC-2023/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import java.util.Arrays;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -24,96 +25,102 @@ import frc.robot.Intake.Rollers;
  * project.
  */
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
 
-  double x;
-  double y;
-  double twist;
-  double joystickAngle;
-  double joystickMag;
-  double fieldCenOffset;
+    double x;
+    double y;
+    double twist;
+    double joystickAngle;
+    double joystickMag;
+    double fieldCenOffset;
 
-  double[] balance_drive = {0, 0};
+    double[] balance_drive = {0, 0};
 
-  @Override
-  public void robotInit() {
-    Map.initialAngle = Map.gyro.getYaw();
-    Arm.initialize();
-  }
+    @Override
+    public void robotInit() {
+        Map.initialAngle = Map.gyro.getYaw();
+        Arm.initialize();
 
-  @Override
-  public void robotPeriodic() {}
-
-  @Override
-  public void autonomousInit() {
-    Map.elapsedTime = 0;
-  }
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {
-    Map.elapsedTime = 0;
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    x = Map.driver.getRawAxis(Map.driverMode[0]) / 3;
-    y = Map.driver.getRawAxis(Map.driverMode[1]) / 3;
-    twist = Map.driver.getRawAxis(Map.driverMode[2]) / 4;
-    joystickAngle = 180 + (Math.atan2(y, -x) / (Math.PI) * 180);
-    joystickMag = Math.sqrt(x * x + y * y);
-    fieldCenOffset = Map.initialAngle - Map.gyro.getYaw();
-
-    if (Map.driver.getPOV() != -1) {
-      DriveTo.goToCoordsPID(0, 0, 1);
-    } else if (Map.driver.getRawButton(1)) {
-      DriveRoute.driveSpeed(SimpleZigZag.path, 3);
-    } else if (Map.driver.getRawButton(5)) {
-      Balance.balanceRobot();
-    } else {
-      Map.swerve.swerveDrive(joystickAngle + fieldCenOffset, joystickMag, twist);
-      Map.swerve.odometry(fieldCenOffset);
+        switch (DriverStation.getAlliance()) {
+            case Blue: Map.isRedAlliance = false;
+            case Red: Map.isRedAlliance = true;
+            case Invalid: Map.isRedAlliance = true;
+        }
+        Map.changePDPLights(true);
     }
 
-    if (Map.driver.getRawButton(6)) {
-      Map.initialAngle = Map.gyro.getYaw();
-      Map.swerve.xPos = 0;
-      Map.swerve.yPos = 0;
-      DriveRoute.index = 0;
-      ShortTestPaths.onBoard = false;
+    @Override
+    public void robotPeriodic() {}
+
+    @Override
+    public void autonomousInit() {
+        Map.elapsedTime = 0;
     }
-    
-    
-    Arm.LiftArm(!Map.driver.getRawButton(4), Map.driver.getRawButton(3));
-    Arm.ArmExtend(Map.driver.getRawButton(2), Map.driver.getRawButton(4));
-    Rollers.roll(Map.driver.getRawAxis(2), Map.driver.getRawAxis(3));
 
-  }
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {
-    if (Map.driver.getRawButtonPressed(9)) {
-      Map.driver.setRumble(RumbleType.kLeftRumble, 1);
-      Map.driver.setRumble(RumbleType.kRightRumble, 1);
-      if (Arrays.equals(Map.driverMode, Map.normalMode)) {
-        Map.driverMode = Map.kateMode;
-        SmartDashboard.putString("Drive mode", "Kate");
-      } else {
-        Map.driverMode = Map.normalMode;
-        SmartDashboard.putString("Drive mode", "Normal");
-      }
-    } else if (Map.driver.getRawButtonReleased(9)) {
-      Map.driver.setRumble(RumbleType.kLeftRumble, 0);
-      Map.driver.setRumble(RumbleType.kRightRumble, 0);
+    @Override
+    public void teleopInit() {
+        Map.elapsedTime = 0;
     }
-  }
+
+    @Override
+    public void teleopPeriodic() {
+        x = Map.driver.getRawAxis(Map.driverMode[0]) / 3;
+        y = Map.driver.getRawAxis(Map.driverMode[1]) / 3;
+        twist = Map.driver.getRawAxis(Map.driverMode[2]) / 4;
+        joystickAngle = 180 + (Math.atan2(y, -x) / (Math.PI) * 180);
+        joystickMag = Math.sqrt(x * x + y * y);
+        fieldCenOffset = Map.initialAngle - Map.gyro.getYaw();
+
+        if (Map.driver.getPOV() != -1) {
+            DriveTo.goToCoordsPID(0, 0, 1);
+        } else if (Map.driver.getRawButton(1)) {
+            DriveRoute.driveSpeed(SimpleZigZag.path, 3);
+        } else if (Map.driver.getRawButton(5)) {
+            Balance.balanceRobot();
+        } else {
+            Map.swerve.swerveDrive(joystickAngle + fieldCenOffset, joystickMag, twist);
+            Map.swerve.odometry(fieldCenOffset);
+        }
+
+        if (Map.driver.getRawButton(6)) {
+            Map.initialAngle = Map.gyro.getYaw();
+            Map.swerve.xPos = 0;
+            Map.swerve.yPos = 0;
+            DriveRoute.index = 0;
+            ShortTestPaths.onBoard = false;
+        }
+        
+        Arm.LiftArm(!Map.driver.getRawButton(4), Map.driver.getRawButton(3));
+        Arm.ArmExtend(Map.driver.getRawButton(2), Map.driver.getRawButton(4));
+        Rollers.roll(Map.driver.getRawAxis(2), Map.driver.getRawAxis(3));
+
+    }
+
+    @Override
+    public void disabledInit() {}
+
+    @Override
+    public void disabledPeriodic() {
+        if (Map.driver.getRawButtonPressed(9)) {
+            Map.driver.setRumble(RumbleType.kLeftRumble, 1);
+            Map.driver.setRumble(RumbleType.kRightRumble, 1);
+            if (Arrays.equals(Map.driverMode, Map.normalMode)) {
+                Map.driverMode = Map.kateMode;
+                SmartDashboard.putString("Drive mode", "Kate");
+            } else {
+                Map.driverMode = Map.normalMode;
+                SmartDashboard.putString("Drive mode", "Normal");
+            }
+        } else if (Map.driver.getRawButtonReleased(9)) {
+            Map.driver.setRumble(RumbleType.kLeftRumble, 0);
+            Map.driver.setRumble(RumbleType.kRightRumble, 0);
+        }
+    }
 
 }
