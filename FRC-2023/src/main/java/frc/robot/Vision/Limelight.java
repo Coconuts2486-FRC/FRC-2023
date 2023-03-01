fe693c7d730e40604f082e53a9982b56271f6f2d
Connorris <norris.connor.j@gmail.com>
2023-02-28 17:29:49 -0700

    Added limelight file

---
+++ FRC-2023/src/main/java/frc/robot/Vision/Limelight.java
@@ -0,0 +1,65 @@
+package frc.robot.Vision;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Map;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+public class Limelight {
+    public static NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    public static NetworkTableEntry tx = table.getEntry("tx");
+    public static NetworkTableEntry ty = table.getEntry("ty");
+    public static NetworkTableEntry ta = table.getEntry("ta");
+  
+   
+    public static double xOffset;
+    public static boolean pipelineOneOn = false; 
+   
+    // change pid vlaues here
+    
+   
+    public static void pipelineZero(){
+        NetworkTableEntry pipelineEntry = table.getEntry("pipeline");
+        pipelineEntry.setNumber(0);
+    
+        }
+
+
+    public static void track (){
+
+    
+    //read values periodically
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+
+    //post to smart dashboard periodically
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+
+    }
+
+    
+ public static PIDController vPid = new PIDController(0.0000000000001, 0, -.0001);
+   
+ public static void pipelineOne(){
+    NetworkTableEntry pipelineEntry = table.getEntry("pipeline");
+    pipelineEntry.setNumber(1);
+
+    }
+ public static double Target(){
+if (Map.driver.getRawButton(2) == true){
+    xOffset = tx.getDouble(0.0);
+}else{
+    xOffset = 0;
+        }
+    return (xOffset/70)+vPid.calculate(xOffset);
+
+    }
+}
+
+
